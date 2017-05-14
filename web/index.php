d91dfafe82598bfc6c2968f66caa665d18c54cbc
al0mie <alex.mokrencko@gmail.com>
2017-05-14 21:53:04 +0300

    addd .htaccess to web directory

--- web/index.php
+++ web/index.php
@@ -2,11 +2,11 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-define("ROOT_PATH", __DIR__ . "/..");
+define('ROOT_PATH', __DIR__ . '/..');
 
 $app = new Silex\Application();
 
-require __DIR__ . '/../resources/config/prod.php';
+require __DIR__ . '/../config/prod.php';
 
 require __DIR__ . '/../src/app.php';
 
