45eceeb3e735cb05243c2b7b1eb0f53e7dfcd3c8
al0mie <alex.mokrencko@gmail.com>
2017-05-14 21:55:25 +0300

    add a base editabe table grid

---
+++ resources/assets/js/components/EditableTableGrid.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="container">
+        <table :class="['table', classes]" v-cloak>
+            <thead>
+                <tr>
+                    <template v-for="field in tableFields">
+                        <th :id="'_' + field.name"
+                            :class="['table-th', field.titleClass]"
+                            v-html="field['title']"
+                        ></th>
+                    </template>
+                </tr>
+            </thead>
+
+            <tbody v-cloak class="vuetable-body">
+                <template v-for="(item, index) in tableData">
+                    <row :item="item" :fields="tableFields" :apiUrl="resourceUrl"
+                          v-on:destroy="destroy"
+                          v-on:update="update"
+                    ></row>
+                </template>
+            </tbody>
+        </table>
+        <br>
+        <create-item-form :fields="tableFields" :apiUrl="apiUrl" v-on:save="save"></create-item-form>
+    </div>
+</template>
+
+<script>
+    import CreateItemForm from './CreateItemForm.vue';
+    import Row from './Row.vue';
+
+    export default {
+        components: {
+            'row': Row,
+            'create-item-form': CreateItemForm
+        },
+        props: {
+            apiUrl: {
+                type: String,
+                required: true
+            },
+            fields: {
+                type: Array,
+                required: true
+            },
+            classes: {
+                type: String,
+                required: false,
+                default: 'table table-grid'
+            }
+        },
+        data() {
+            return {
+                tableFields: [],
+                tableData: [],
+                resourceUrl: ''
+            }
+        },
+        mounted() {
+            this.normalizeFields();
+            this.normalizeUrl();
+            this.loadData();
+        },
+        methods: {
+            normalizeUrl() {
+                if (typeof(this.apiUrl) === 'undefined') {
+                    console.error('You need to provide "api url" prop.');
+                    return;
+                }
+                this.resourceUrl = this.apiUrl.trim('/');
+                this.resourceUrl = this.apiUrl + '/';
+            },
+            normalizeFields () {
+                this.tableFields = [];
+
+                if (typeof(this.fields) === 'undefined') {
+                    console.error('You need to provide "fields" prop.');
+                    return;
+                }
+
+                let self = this;
+                let obj;
+                this.fields.forEach(function (field, i) {
+                    if (typeof (field) === 'string') {
+                        obj = {
+                            name: field,
+                            title: field,
+                            titleClass: ''
+                        }
+                    } else {
+                        obj = {
+                            name: field.name,
+                            title: (field.title === undefined) ? field.field.name : field.title,
+                            titleClass: (field.titleClass === undefined) ? '' : field.titleClass
+                        }
+                    }
+                    self.tableFields.push(obj)
+                })
+            },
+
+            loadData (success = this.loadSuccess, failed = this.loadFailed) {
+                axios.get(this.apiUrl).then(success, failed);
+            },
+
+            loadSuccess (response) {
+                this.tableData = response.data;
+            },
+            loadFailed (response) {
+                console.error('load-error', response);
+            },
+
+            destroy: function (item) {
+                let index = this.tableData.indexOf(item);
+                this.tableData.splice(index, 1);
+            },
+            update: function (item) {
+                let index = this.tableData.indexOf(item);
+                this.tableData[index] = item;
+            },
+            save: function (item) {
+                this.tableData.push(item);
+            }
+        }
+    }
+</script>
---
+++ resources/assets/js/components/Example.vue
@@ -0,0 +1,46 @@
+<template>
+   <div>
+       <editable-table-grid :apiUrl="apiUrl"
+                 :fields="fields"
+                 :classes="classes"
+       ></editable-table-grid>
+   </div>
+</template>
+
+<script>
+    import EditableTableGrid from './EditableTableGrid.vue'
+
+    export default {
+        components: {
+            'editable-table-grid': EditableTableGrid
+        },
+        data() {
+            return {
+                apiUrl: '/api/users',
+                classes: 'table table-bordered',
+                fields: [
+                    {
+                        title: 'Username',
+                        name: 'username'
+                    },
+                    {
+                        title: 'Email',
+                        name: 'email'
+                    },
+                    {
+                        title: 'First name',
+                        name: 'first_name'
+                    },
+                    {
+                        title: 'Last name',
+                        name: 'last_name'
+                    },
+                    {
+                        title: 'address',
+                        name: 'address'
+                    }
+                ]
+            }
+        }
+    }
+</script>
