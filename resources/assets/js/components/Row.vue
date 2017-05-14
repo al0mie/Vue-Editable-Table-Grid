202477712ea87352e1486282dcef4c73a1ae60c8
al0mie <alex.mokrencko@gmail.com>
2017-05-14 22:56:31 +0300

    fix row of the table

---
+++ resources/assets/js/components/Row.vue
@@ -0,0 +1,83 @@
+<template>
+    <tr>
+        <template v-for="(field, index) in fields">
+            <td>
+                <input type="text"  class="form-control"
+                       v-model="item[field.name]" v-if="editMode"
+                >
+                <span v-else>{{ item[field.name] }}</span>
+            </td>
+        </template>
+        <td>
+            <button type="button" class="btn btn-info"
+                    v-on:click="edit" v-if="!editMode"
+            >
+                Edit
+            </button>
+
+            <button type="button" class="btn btn-default"
+                    v-on:click="cancelEdit" v-if="editMode"
+            >
+                Cancel
+            </button>
+
+            <button type="button" class="btn btn-primary"
+                    v-on:click="update(item, editForm)" v-if="editMode"
+            >
+                update
+            </button>
+
+            <button type="button" class="btn btn-danger"
+                    v-on:click="destroy(item)" v-if="!editMode"
+            >
+                Delete
+            </button>
+        </td>
+    </tr>
+</template>
+
+<script>
+    export default {
+        props:['item', 'fields', 'apiUrl'],
+        data() {
+            return {
+                editMode: false,
+                editForm: {}
+            }
+        },
+        methods: {
+            edit() {
+                this.editMode = true;
+                let self = this;
+
+                this.fields.forEach(function (field, i) {
+                    self.editForm[field.name] = self.item[field.name];
+                });
+            },
+            cancelEdit() {
+                this.editMode = false;
+                let self = this;
+                this.fields.forEach(function (field, i) {
+                    self.editForm[field.name] = '';
+                });
+            },
+            update(oldItem, newItem) {
+                this.edit();
+                axios.patch(this.apiUrl + oldItem.id, newItem).then(response => {
+                    console.log(response.data);
+                    this.$emit('update', newItem);
+                    this.cancelEdit();
+                }, (response) => {
+                    alert('Invalid data');
+                });
+            },
+            destroy(item) {
+                axios.delete(this.apiUrl + item.id).then(response => {
+                    this.$emit('destroy', item);
+            }, (response) => {
+                    alert('Invalid data');
+                });
+            },
+        }
+    }
+</script>
