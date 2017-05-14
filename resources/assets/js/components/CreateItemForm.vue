c78d8d809b633cc280e7cd9c8a56f441d5583a19
al0mie <alex.mokrencko@gmail.com>
2017-05-14 23:22:26 +0300

    add base form of creating a new record

---
+++ resources/assets/js/components/CreateItemForm.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <button class="btn btn-success" v-on:click="edit" v-if="!create">Add</button>
+        <button class="btn btn-success" v-on:click="save" v-if="create">Save</button>
+        <button class="btn btn-primary" v-on:click="cancel" v-if="create">Cancel</button>
+
+        <form v-if="create">
+            <div v-for="(field, index) in fields">
+                <label :for="field.name">{{ field.title }}</label>
+                    <input type="text"  class="form-control"
+                           v-model="createForm[field.name]"
+                           :id="field.name"
+                    >
+                </div>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            fields : {
+                type: Array,
+                required: true
+            },
+            apiUrl: {
+                type: String,
+                required: true
+            }
+        },
+        data() {
+            return {
+                create: false,
+                createForm: {}
+            }
+        },
+        methods: {
+            edit: function() {
+                console.log(this.fields);
+                this.create = true;
+            },
+
+            save: function() {
+                axios.post(this.apiUrl, this.createForm).then(response => {
+                    console.log(response.data);
+                    this.$emit('save', response.data);
+                    this.cancel();
+                }, (response) => {
+                    alert('Invalid data');
+                });
+            },
+
+            cancel: function() {
+                this.create = false;
+                this.createForm = {};
+            }
+        }
+    }
+</script>
